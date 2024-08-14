454985eecfc584c3282874f189f30dc66dd55eb3
zaheenrakib <rakibislambest@gmail.com>
2024-08-15 02:10:31 +0600

    Fix Some Bug

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import Sidebar from './Sidebar/Sidebar';
 import { Outlet } from 'react-router-dom';
-import DashBord from './DashBoard/DashBord';
-
 
 const Main = () => {
     return (
         <div className='flex'>
             <Sidebar></Sidebar>
             <Outlet></Outlet>
-            <DashBord></DashBord>
         </div>
     );
 };
--- src/Layout/Sidebar/Sidebar.jsx
+++ src/Layout/Sidebar/Sidebar.jsx
@@ -5,19 +5,21 @@ import { GiCartwheel } from 'react-icons/gi';
 import { IoSettings, IoSunnyOutline } from 'react-icons/io5';
 import { MdDashboard } from 'react-icons/md';
 import { Link } from 'react-router-dom';
+import DashBord from '../DashBoard/DashBord';
 
 const Sidebar = () => {
     return (
-        <div>
-            <div className='w-[250px] h-full bg-slate-200'>
+        <div className='flex'>
+            <div className='w-[250px] h-screen fixed bg-slate-200'>
                 <div className='w-full flex justify-center items-center'>
                     <IoSunnyOutline className='text-4xl mt-4 text-blue-300' />
-                    <h1 className='text-2xl mt-4 font-extrabold'><span className='text-blue-300 font-semibold'>On</span>Course</h1>
+                    <h1 className='text-2xl mt-4 font-extrabold'>
+                        <span className='text-blue-300 font-semibold'>On</span>Course
+                    </h1>
                 </div>
 
                 <ul>
-
-                    <div className='flex  flex-col gap-y-72 mt-8 p-2 text-black'>
+                    <div className='flex flex-col mt-8 p-2 text-black'>
                         <div className='space-y-8'>
                             <li className='flex justify-start items-center text-blue-400 gap-2 text-xl'>
                                 <MdDashboard />
@@ -32,20 +34,24 @@ const Sidebar = () => {
                                 <Link to="product-list" className=''>Financial Aid</Link>
                             </li>
                         </div>
-                        <div className='space-y-2'>
-                            <li className='flex justify-start items-center gap-2 text-xl' >
-                            <FcSupport />
+                        <div className='space-y-2 absolute bottom-2'>
+                            <li className='flex justify-start items-center gap-2 text-xl'>
+                                <FcSupport />
                                 <Link to="product-list" className=''>Support</Link>
                             </li>
                             <li className='flex justify-start items-center gap-2 text-xl'>
-                            <IoSettings />
+                                <IoSettings />
                                 <Link to="product-list" className=''>Setting</Link>
                             </li>
                         </div>
                     </div>
                 </ul>
             </div>
+            <div className='flex-1 ml-[240px] p-4'>
+                <DashBord></DashBord>
+            </div>
         </div>
+
     );
 };
 
